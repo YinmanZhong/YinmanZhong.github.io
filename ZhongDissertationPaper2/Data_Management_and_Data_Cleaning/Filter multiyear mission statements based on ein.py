d0620b74c0d9fcf767727eb1f1d8e6062323e5a0
Yinman Zhong <yinmanzhong@gmail.com>
2025-01-06 12:49:05 -0500

    Add files via upload

---
+++ ZhongDissertationPaper2/Data_Management_and_Data_Cleaning/Filter multiyear mission statements based on ein.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Load the Excel and CSV files
+excel_file_path = '/Users/yinmanzhong/Desktop/Dissertation/Data/Candid Academic Data/Excel Versions/Main_2018_Added_Simplified_EIN_(E20+E21+E22).xlsx'
+csv_file_path = '/Users/yinmanzhong/Desktop/Dissertation/1. My Dissertation Data/Mission Statements Extracted from Giving Tuesday (E20+E22)/Missions_E20+E21+E22/2024_06_21_All_Missions_Raw.csv'
+
+# Read the Excel file (assuming the EIN column is named 'ein' in the Excel file)
+excel_df = pd.read_excel(excel_file_path)
+ein_list = excel_df['FILEREIN'].tolist()  # Create a list of EINs to match
+
+# Read the CSV file (assuming the EIN column is named 'ein' in the CSV file)
+csv_df = pd.read_csv(csv_file_path, dtype={'TAXYEAR': 'Int64'}, low_memory=False)
+
+# Filter the CSV data to include only rows with EINs present in the Excel file
+filtered_df = csv_df[csv_df['FILEREIN'].isin(ein_list)]
+
+# Define the output path for the filtered CSV file
+output_path = ('/Users/yinmanzhong/Desktop/Dissertation/1. My Dissertation Data/Mission Statements Extracted from '
+               'Giving Tuesday (E20+E22)/Missions_E20+E21+E22/2024_06_21_All_Missions_E20+E21+E22.xlsx')
+
+# Save the filtered data to the specified local folder
+filtered_df.to_excel(output_path, index=False)
+
